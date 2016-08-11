b6bc304f506fcd23f9d173dff422ae5ed2fdb1a6
juzar4u <juzar4u@hotmail.com>
2016-08-11 14:35:12 +0400

    commit

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from webapp import app, config
 ### Run the App             ###
 ###############################
 if __name__ == "__main__":
-    app.theapp.run(host=config.DevConfig.HOST, port=config.DevConfig.PORT)
+    app.theapp.run(host=config.DevConfig.HOST)
 	
 	###########################
 ### Git Test by Juzar           ###
