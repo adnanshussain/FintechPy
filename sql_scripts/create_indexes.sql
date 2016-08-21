0e5c120e76f0a24dad0bd7eff78e93f2932d5a4a
Adnan <adnanshussain@gmail.com>
2016-08-21 09:46:11 +0400

    - updated uwsgi to run the webapp module directly - drop and create indices as currently created indices not working when deployed on production

--- sql_scripts/create_indexes.sql
+++ sql_scripts/create_indexes.sql
@@ -1,4 +1,5 @@
-DROP INDEX idx_sp_dt_setid;
+DROP INDEX idx_sp_dt_asc_setid_asc;
+DROP INDEX idx_sp_dt_desc_setid_asc;
 
 CREATE INDEX idx_sp_dt_asc_setid_asc on stock_prices (for_date, stock_entity_type_id);
 CREATE INDEX idx_sp_dt_desc_setid_asc on stock_prices (for_date desc, stock_entity_type_id);
